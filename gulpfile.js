c458862a533b0d2f9d30d7f6a83f294fa19f7463
Corinna Kuhn <corinnak@qualtrics.com>
2018-11-16 08:36:11 -0800

    chore(): add ESlint

--- gulpfile.js
+++ gulpfile.js
@@ -10,27 +10,25 @@ const parallelTasks = require('./config/parallel');
 const seriesTasks = require('./config/series');
 
 Object.entries(commands).forEach(([name, config]) => {
-    log.debug(`Registering command '${name}'`.grey);
-    const command = new Command(name, config);
-    gulp.task(name, command.getTask());
+  log.debug(`Registering command '${name}'`.grey);
+  const command = new Command(name, config);
+  gulp.task(name, command.getTask());
 });
 
-
 Object.entries(seriesTasks).forEach(([name, tasks]) => {
-    log.debug(`Registering series alias '${name}':`.grey + stringify(tasks));
-    gulp.task(name, gulp.series(...tasks));
+  log.debug(`Registering series alias '${name}':`.grey + stringify(tasks));
+  gulp.task(name, gulp.series(...tasks));
 });
 
 Object.entries(parallelTasks).forEach(([name, tasks]) => {
-    log.debug(`Registering parallel alias '${name}':`.grey + stringify(tasks));
-    gulp.task(name, gulp.parallel(...tasks));
+  log.debug(`Registering parallel alias '${name}':`.grey + stringify(tasks));
+  gulp.task(name, gulp.parallel(...tasks));
 });
 
-
 function list() {
-    log.info(`All available (individual) commands: ${stringify(Object.keys(commands))}`);
-    log.info(`All available parallel alias commands: ${stringify(parallelTasks)}`);
-    log.info(`All available series alias commands: ${stringify(seriesTasks)}`);
+  log.info(`All available (individual) commands: ${stringify(Object.keys(commands))}`);
+  log.info(`All available parallel alias commands: ${stringify(parallelTasks)}`);
+  log.info(`All available series alias commands: ${stringify(seriesTasks)}`);
 }
 
 gulp.task(list);
--- index.js
+++ index.js
@@ -7,28 +7,30 @@ const argv = require('yargs').argv;
 const colors = require('colors');
 
 const logLevelColors = {
-    TRACE: colors.grey,
-    DEBUG: colors.magenta,
-    INFO: colors.blue,
-    WARN: colors.yellow,
-    ERROR: colors.red
+  TRACE: colors.grey,
+  DEBUG: colors.magenta,
+  INFO: colors.blue,
+  WARN: colors.yellow,
+  ERROR: colors.red
 };
 
 process.env.LOG_LEVEL = 'info';
 
-Object.keys(logLevelColors).forEach(logLevel => {
-    if (argv[logLevel.toLowerCase()]) {
-        process.env.LOG_LEVEL = logLevel;
-    }
+Object.keys(logLevelColors).forEach((logLevel) => {
+  if (argv[logLevel.toLowerCase()]) {
+    process.env.LOG_LEVEL = logLevel;
+  }
 });
 
 log.setLevel(process.env.LOG_LEVEL);
 
 logPrefix.reg(log);
 logPrefix.apply(log, {
-    format: (level, name, timestamp)  => {
-        return `${colors.grey(`[${timestamp}]`)} ${logLevelColors[level.toUpperCase()](level.padEnd(5))} ${colors.cyan(`[${name}]`)}`;
-    }
+  format: (level, name, timestamp) => {
+    return `${colors.grey(`[${timestamp}]`)} ${logLevelColors[level.toUpperCase()](level.padEnd(5))} ${colors.cyan(
+      `[${name}]`
+    )}`;
+  }
 });
 
 log.info(`Welcome to your awesome dev process manager! Now you can just sit back and relax (er, code)!`.green);
@@ -38,8 +40,7 @@ log.info(`Log level set to ${process.env.LOG_LEVEL}`);
 require('./gulpfile');
 
 if (argv._.length === 0) {
-    log.error(`No command specified, exiting.`);
+  log.error(`No command specified, exiting.`);
 } else {
-    gulp.task(argv._[0])();
+  gulp.task(argv._[0])();
 }
-
