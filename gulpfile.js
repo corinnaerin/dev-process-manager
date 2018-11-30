e530df1b97d862bd2ec49d65b79e91bf65d6cd13
Corinna Kuhn <corinnak@qualtrics.com>
2018-11-30 13:25:47 -0800

    fix(): user-friendly error message when trying to register a command with a reserved name

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,39 @@ const commands = require('./config/commands');
 const parallelTasks = require('./config/parallel');
 const seriesTasks = require('./config/series');
 
+const reservedNames = new Set(['help', 'list', 'default']);
+
+function validateName(name) {
+  if (reservedNames.has(name)) {
+    log.warn(
+      `"${name}" is a reversed command name, skipping registration. Please rename your command to something else!`
+    );
+    return false;
+  }
+  return true;
+}
+
 Object.entries(commands).forEach(([name, config]) => {
+  if (!validateName(name)) {
+    return;
+  }
   log.debug(`Registering command '${name}'`.grey);
   const command = new Command(name, config);
   gulp.task(name, command.getTask());
 });
 
 Object.entries(seriesTasks).forEach(([name, tasks]) => {
+  if (!validateName(name)) {
+    return;
+  }
   log.debug(`Registering series alias '${name}':`.grey + stringify(tasks));
   gulp.task(name, gulp.series(...tasks));
 });
 
 Object.entries(parallelTasks).forEach(([name, tasks]) => {
+  if (!validateName(name)) {
+    return;
+  }
   log.debug(`Registering parallel alias '${name}':`.grey + stringify(tasks));
   gulp.task(name, gulp.parallel(...tasks));
 });
